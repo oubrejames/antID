dcd5ab46af340d2d3d9f33e1b35b068f58f23f87
James Oubre <oubrejames@gmail.com>
2023-05-17 21:34:16 -0500

    Frankensteined blackfly examples into a class to get opencv frames from blackfly camera

--- black_fly_tools/include/black_fly_lib.hpp
+++ black_fly_tools/include/black_fly_lib.hpp
@@ -4,6 +4,11 @@
 /// \brief Two-dimensional rigid body transformations.
 
 #include "Spinnaker.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+namespace bfc
+{
 
 class BlackFlyCamera
 {
@@ -13,9 +18,20 @@ class BlackFlyCamera
         Spinnaker::CameraPtr pCam;
         
     public:
+        BlackFlyCamera();
+        ~BlackFlyCamera();
+        
         Spinnaker::ImagePtr pResultImage;
 
-
+        int set_continuous_acquisition();
+        cv::Mat get_frame();
+        
+        /*
+        Gonna have to think about this more but I want this class to:
+            1) Set up the camera object
+            2) Make sure the camera is set to continuous acquisition
+            3) Output frames in opencv format
+        */
+};
 }
-
 #endif
--- black_fly_tools/src/black_fly_lib.cpp
+++ black_fly_tools/src/black_fly_lib.cpp
@@ -1,14 +1,50 @@
 #include "Spinnaker.h"
-#include "black_fly_lib.hpp"
+#include <black_fly_lib.hpp>
+
+namespace bfc{
 
 // Create a BlackFlyCamera constructer 
-BlackFlyCamera::BlackFlyCamera(): 
-    system{Spinnaker::System::GetInstance();}, 
-    camList{system->GetCameras();}, 
-    pCam{camList.GetByIndex(0)},
-    pResultImage{pCam->GetNextImage()}{}
+BlackFlyCamera::BlackFlyCamera(){
+    system = Spinnaker::System::GetInstance();
+    camList = system->GetCameras(); 
+    pCam = camList.GetByIndex(0);
+    pResultImage = pCam->GetNextImage();
+}
 
 // Create a BlackFlyCamera destructor
-BlackFlyCamera::~BlackFlyCamera() {
+BlackFlyCamera::~BlackFlyCamera(){
     pResultImage->Release();
+    camList.Clear();
+    system->ReleaseInstance();
+}
+
+int BlackFlyCamera::set_continuous_acquisition(){
+        Spinnaker::GenApi::INodeMap & nodeMap = pCam->GetNodeMap();
+        Spinnaker::GenApi::CEnumerationPtr ptrAcquisitionMode = nodeMap.GetNode("AcquisitionMode");
+        if (!Spinnaker::GenApi::IsAvailable(ptrAcquisitionMode) || !Spinnaker::GenApi::IsWritable(ptrAcquisitionMode))
+        {
+            std::cout << "Unable to set acquisition mode to continuous (enum retrieval). Aborting..." << std::endl << std::endl;
+            return -1;
+        }
+
+        // Retrieve entry node from enumeration node
+        Spinnaker::GenApi::CEnumEntryPtr ptrAcquisitionModeContinuous = ptrAcquisitionMode->GetEntryByName("Continuous");
+        if (!Spinnaker::GenApi::IsAvailable(ptrAcquisitionModeContinuous) || !Spinnaker::GenApi::IsReadable(ptrAcquisitionModeContinuous))
+        {
+            std::cout << "Unable to set acquisition mode to continuous (entry retrieval). Aborting..." << std::endl << std::endl;
+            return -1;
+        }
+
+        // Retrieve integer value from entry node
+        const int64_t acquisitionModeContinuous = ptrAcquisitionModeContinuous->GetValue();
+
+        // Set integer value from entry node as new value of enumeration node
+        ptrAcquisitionMode->SetIntValue(acquisitionModeContinuous);
+}
+
+cv::Mat BlackFlyCamera::get_frame(){
+    pResultImage = pCam->GetNextImage();
+    cv::Mat frame = cv::Mat(pResultImage->GetHeight(), pResultImage->GetWidth(), (pResultImage->GetNumChannels() == 3) ? CV_8UC3 : CV_8UC1, pResultImage->GetData(), pResultImage->GetStride());
+    return frame;
+}
 }
