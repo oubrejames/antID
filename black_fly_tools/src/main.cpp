836975e8aad19b1d5d74bede597a9a8fec1b6abb
James Oubre <oubrejames@gmail.com>
2023-05-22 11:28:18 -0500

    Commiting working stream

--- black_fly_tools/src/main.cpp
+++ black_fly_tools/src/main.cpp
@@ -17,6 +17,7 @@ int main() {
 
 // Initialize the camera
     pCam->Init();
+
     pCam->BeginAcquisition();
 
     std::cout << "Get next image" << std::endl;
@@ -24,12 +25,11 @@ int main() {
     pResultImage->Release();
     std::cout << "Got next image" << std::endl;
 
-    cv::Mat frame = cv::Mat(pResultImage->GetHeight(), pResultImage->GetWidth(), (pResultImage->GetNumChannels() == 3) ? CV_8UC3 : CV_8UC1, pResultImage->GetData(), pResultImage->GetStride());
-
+    cv::namedWindow("Video", cv::WINDOW_NORMAL);
+    cv::resizeWindow("Video", 800, 600);
 
   while (true) {
     pResultImage = pCam->GetNextImage(1000);
-    std::cout << "YAGA" << std::endl;
     cv::Mat frame = cv::Mat(pResultImage->GetHeight(), pResultImage->GetWidth(), (pResultImage->GetNumChannels() == 3) ? CV_8UC3 : CV_8UC1, pResultImage->GetData(), pResultImage->GetStride());
     pResultImage->Release();
     if (frame.empty()) {
@@ -48,47 +48,3 @@ int main() {
 
   return 0;
 }
-
-// int main() {
-
-//     // Create a BlackFlyCamera object
-//     std::cout << "Create camera object" << std::endl;
-//     // auto system = Spinnaker::System::GetInstance();
-//     // auto camList = system->GetCameras(); 
-//     // auto pCam = camList.GetByIndex(0);
-
-//     // std::cout << "Number of cameras detected: " << camList.GetSize() << std::endl << std::endl;
-
-//     bfc::BlackFlyCamera cam;
-
-//     // Set the camera to continuous acquisition mode
-//     std::cout << "Camera object created" << std::endl;
-//     // std::cout << "Set camera to continuous acquisition" << std::endl;
-//     // cam.set_continuous_acquisition();
-//     // std::cout << "Continuous acquisition set" << std::endl;
-
-
-
-//   while (true) {
-//     cv::Mat frame;
-//     std::cout << "Capture frame from camera" << std::endl;
-
-//     frame = cam.get_frame(); // Read a new frame from the camera
-//     std::cout << "Got frame" << std::endl;
-
-//     if (frame.empty()) {
-//       std::cout << "Failed to capture a frame." << std::endl;
-//       break;
-//     }
-
-//     cv::imshow("Video", frame); // Display the frame
-
-//     // Exit the loop if the 'Esc' key is pressed
-//     if (cv::waitKey(1) == 27)
-//       break;
-//   }
-
-//   cv::destroyAllWindows(); // Close all windows
-
-//   return 0;
-// }
