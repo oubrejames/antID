94abcdb5905bbda61ea6ed246d5b038058528979
James Oubre <oubrejames@gmail.com>
2023-05-29 21:44:42 -0500

    Updated comments for data collect

--- antID/src/collect_data.cpp
+++ antID/src/collect_data.cpp
@@ -5,6 +5,7 @@
 using namespace bfc;
 
 int main(){
+    // Get ID number of ant to start pause_flaging
     std::cout << "Enter starting ant ID number:";
     int curr_id;
     std::cin >> curr_id;
@@ -33,8 +34,7 @@ int main(){
     std::string outputFile = "ant_" + std::to_string(curr_id) + ".avi";
     cv::VideoWriter writer(outputFile, cv::VideoWriter::fourcc('M','J','P','G'), fps, frameSize);
 
-    // Open the video file for writing
-    // writer.open(outputFile, fourcc, fps, frameSize);
+    // Check if the video writer is open
     if (!writer.isOpened())
     {
         // Error handling if the video writer cannot be opened
@@ -42,20 +42,20 @@ int main(){
         return -1;
     }
 
-    bool flag = true;
-    bool record = true;
-    while (flag)
+    bool end_flag = true; // end_flag to end pause_flaging
+    bool pause_flag = true; // end_flag to pause pause_flaging
+    while (end_flag)
     {
-        if (record){
+        if (pause_flag){
+            // Get frame from camera
             frame = camera.get_frame();
-            // cap >> frame;
             cv::imshow("Live Feed", frame); // Display the frame
             writer.write(frame);
         }
 
-        // Press Space to pause recording
+        // Press Space to pause pause_flaging
         if (cv::waitKey(1) == 32){
-            std::cout << "You have paused recording" << std::endl;
+            std::cout << "You have paused pause_flaging" << std::endl;
             std::cout << "Press:" << std::endl;
             std::cout << "    'q' to quit session" << std::endl;
             std::cout << "    'n' to start new video" << std::endl;
@@ -66,16 +66,19 @@ int main(){
 
             if (input == "q"){
                 std::cout << "You have ended the session" << std::endl;
-                flag = false;
+                end_flag = false;
             }
             else if (input == "n"){
                 std::cout << "You have created a new video" << std::endl;
+
+                // Increment ant ID number and update output file name
                 curr_id++;
                 outputFile = "ant_" + std::to_string(curr_id) + ".avi";
+
+                // Release current video writer and create new one
                 writer.release();
-                // frame = camera.get_frame();
                 writer = cv::VideoWriter(outputFile, cv::VideoWriter::fourcc('M','J','P','G'), fps, frameSize);
-                record = true;
+                pause_flag = true;
             }
             else if (input == "c"){
                 continue;
@@ -83,26 +86,10 @@ int main(){
             else{
                 std::cout << "Invalid input" << std::endl;
             }
-            // flag = false;
         }
-
-        // // Stop recording but don't exit the loop if the 's' key is pressed
-        // if (cv::waitKey(1) == 115){
-        //     std::cout << "You have stopped recording" << std::endl;
-        //     record = false;
-        // }
-
-        // // Create a new video if the 'n' key is pressed
-        // if (cv::waitKey(1) == 110){
-        //     std::cout << "You have created a new video" << std::endl;
-        //     curr_id++;
-        //     outputFile = "ant_" + std::to_string(curr_id) + ".avi";
-        //     writer.release();
-        //     writer = cv::VideoWriter(outputFile, cv::VideoWriter::fourcc('M','J','P','G'), fps, frameSize);
-        //     record = true;
-        // }
     }
-    std::cout << "Recording Ended" << std::endl;
+
+    std::cout << "pause_flaging Ended" << std::endl;
     writer.release();
 
     cv::destroyAllWindows();
