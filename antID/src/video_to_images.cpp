e12c73add7fc3da9fc435355111ed2254f3ea0de
James Oubre <oubrejames@gmail.com>
2023-06-27 11:14:53 -0500

    Tuned threshold

--- antID/src/video_to_images.cpp
+++ antID/src/video_to_images.cpp
@@ -13,6 +13,10 @@
 #define IMAGE_WIDTH 2048
 #define PATH_TO_VIDEOS "../../labeled_vids"
 
+//////////////////////////////////////////
+// REMEMBER TO TUNE BOUNDING BOX % AREA //
+//////////////////////////////////////////
+
 bool detect_full_body(std::vector<cv::Point> points, cv::Mat* frame){
     // Get bounding box around ant and filter if not full ant
     if (points.size() > 0){
@@ -39,9 +43,9 @@ bool detect_full_body(std::vector<cv::Point> points, cv::Mat* frame){
         auto percent_area = static_cast<float>(bounding_box_area) / static_cast<float>(image_area);
 
         // Filter if bounding box area is less than 22.35%  and greater than 70% of image area
-        if (percent_area > 0.2235 && percent_area < 0.7){
+        if (percent_area > 0.26 && percent_area < 0.8){
             // Draw bounding box around ant
-            // cv::rectangle(*frame, cv::Point(leftmost_x, topmost_y), cv::Point(rightmost_x, bottommost_y), cv::Scalar(0, 255, 0), 5);
+            cv::rectangle(*frame, cv::Point(leftmost_x, topmost_y), cv::Point(rightmost_x, bottommost_y), cv::Scalar(0, 255, 0), 5);
             return true;
         }
         else{
@@ -58,6 +62,7 @@ int main(){
     {
         // Open video
         cv::VideoCapture cap(video_path.path());
+
         std::cout << "Opening video : " << video_path.path() << std::endl;
 
         auto ant_id = video_path.path().stem().string().back();
@@ -81,6 +86,7 @@ int main(){
 
         // Make directory for labeled images
         std::string dir_labeled_imgs = "../../labeled_images/ant_";
+
         dir_labeled_imgs.push_back(ant_id);
         std::filesystem::create_directory(dir_labeled_imgs, labeled_videos);
         std::cout << "Directory created : " << dir_labeled_imgs << std::endl;
@@ -92,12 +98,17 @@ int main(){
             cv::Mat frame;
             cap >> frame;
 
+            if (frame.empty()){
+                break;
+            }
+
             // Blur frame
-            cv::GaussianBlur(frame, frame, cv::Size(25, 25), 10);
+            cv::Mat blurred_frame;
+            cv::GaussianBlur(frame, blurred_frame, cv::Size(25, 25), 10);
 
             // Apply background subtraction
             cv::Mat fg_mask;
-            pBackSub->apply(frame, fg_mask);
+            pBackSub->apply(blurred_frame, fg_mask);
 
             // Threshold mask
             cv::threshold(fg_mask, fg_mask, 250, 255, cv::THRESH_BINARY);
@@ -112,20 +123,19 @@ int main(){
             std::vector<cv::Point> white_points;
             cv::findNonZero(fg_mask, white_points);
 
-            // TODO
             // If a full ant is detected, save the frame 
             if (detect_full_body(white_points, &frame)){
                 // Save frame
-                cv::imwrite(dir_labeled_imgs + "_im_" + std::to_string(img_count) + ".jpg", frame);
+                cv::imwrite(dir_labeled_imgs + "/ant_" + std::to_string(img_count) + "_im_" + std::to_string(img_count) + ".jpg", frame);
                 img_count++;
             }
 
-            // // Create and resize windows
-            // cv::namedWindow("Frame", cv::WINDOW_NORMAL);
-            // cv::resizeWindow("Frame", 800, 600);
+            // Create and resize windows
+            cv::namedWindow("Frame", cv::WINDOW_NORMAL);
+            cv::resizeWindow("Frame", 800, 600);
 
-            // // Display frame
-            // cv::imshow("Frame", frame);
+            // Display frame
+            cv::imshow("Frame", frame);
 
             // Press  ESC on keyboard to exit
             char c=(char)cv::waitKey(25);
@@ -133,6 +143,7 @@ int main(){
                 break;
             }
         }
+    std::cout << "Processed video : " << video_path.path() << std::endl << std::endl;
     }
     return 0;
 }
