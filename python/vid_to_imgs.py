ea190de4618d9dfc733c38082ef4ba0eed76a18c
James Oubre <oubrejames@gmail.com>
2023-06-30 13:42:25 -0500

    Switching from roboflow api to yolo

--- python/vid_to_imgs.py
+++ python/vid_to_imgs.py
@@ -14,7 +14,7 @@ model= YOLO("YOLO_V8/runs/detect/yolov8s_v8_25e5/weights/best.pt")
 def get_head_box(im):
     # detections = model.predict(im, confidence=60, overlap=30).json()
     detections = model(im, conf=0.8)
-    bboxes = detections[0].boxes.numpy()
+    bboxes = detections[0].boxes
     print('bbox: ', bboxes)
     print("bbox type: ", type(bboxes))
     print('As np', bboxes.xyxy)
