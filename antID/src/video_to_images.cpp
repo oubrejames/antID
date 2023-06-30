107d7bfd6e39a4d32c4f77467d68f02840aa0140
James Oubre <oubrejames@gmail.com>
2023-06-30 09:27:58 -0500

    Small changes to TUI

--- antID/src/video_to_images.cpp
+++ antID/src/video_to_images.cpp
@@ -13,9 +13,8 @@
 #define IMAGE_WIDTH 2048
 #define PATH_TO_VIDEOS "../../labeled_vids"
 
-//////////////////////////////////////////
-// REMEMBER TO TUNE BOUNDING BOX % AREA //
-//////////////////////////////////////////
+#include <chrono>
+using namespace std::chrono;
 
 bool detect_full_body(std::vector<cv::Point> points, cv::Mat* frame){
     // Get bounding box around ant and filter if not full ant
@@ -42,10 +41,10 @@ bool detect_full_body(std::vector<cv::Point> points, cv::Mat* frame){
         // Get percent of bounding box area to image area
         auto percent_area = static_cast<float>(bounding_box_area) / static_cast<float>(image_area);
 
-        // Filter if bounding box area is less than 22.35%  and greater than 70% of image area
+        // Filter if bounding box area is less than 26%  and greater than 70% of image area
         if (percent_area > 0.26 && percent_area < 0.8){
             // Draw bounding box around ant
-            cv::rectangle(*frame, cv::Point(leftmost_x, topmost_y), cv::Point(rightmost_x, bottommost_y), cv::Scalar(0, 255, 0), 5);
+            // cv::rectangle(*frame, cv::Point(leftmost_x, topmost_y), cv::Point(rightmost_x, bottommost_y), cv::Scalar(0, 255, 0), 5);
             return true;
         }
         else{
@@ -59,7 +58,10 @@ int main(){
     const std::filesystem::path labeled_videos{PATH_TO_VIDEOS};
 
     for (auto const& video_path : std::filesystem::directory_iterator{labeled_videos}) 
-    {
+    {   
+        // Start timer
+        auto start = high_resolution_clock::now();
+
         // Open video
         cv::VideoCapture cap(video_path.path());
 
@@ -126,24 +128,29 @@ int main(){
             // If a full ant is detected, save the frame 
             if (detect_full_body(white_points, &frame)){
                 // Save frame
-                cv::imwrite(dir_labeled_imgs + "/ant_" + std::to_string(img_count) + "_im_" + std::to_string(img_count) + ".jpg", frame);
+                cv::imwrite(dir_labeled_imgs + "/ant_" + std::to_string(ant_id) + "_im_" + std::to_string(img_count) + ".jpg", frame);
                 img_count++;
             }
 
-            // Create and resize windows
-            cv::namedWindow("Frame", cv::WINDOW_NORMAL);
-            cv::resizeWindow("Frame", 800, 600);
+            // // Create and resize windows
+            // cv::namedWindow("Frame", cv::WINDOW_NORMAL);
+            // cv::resizeWindow("Frame", 800, 600);
 
-            // Display frame
-            cv::imshow("Frame", frame);
+            // // Display frame
+            // cv::imshow("Frame", frame);
 
-            // Press  ESC on keyboard to exit
-            char c=(char)cv::waitKey(25);
-            if(c==27){
-                break;
-            }
+            // // Press  ESC on keyboard to exit
+            // char c=(char)cv::waitKey(25);
+            // if(c==27){
+            //     break;
+            // }
         }
-    std::cout << "Processed video : " << video_path.path() << std::endl << std::endl;
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<seconds>(stop - start);
+    std::cout << "Processing time:" << duration.count() << std::endl;
+    std::cout << "Processed video: " << video_path.path() << std::endl << std::endl;
+    std::cout << "Number of images saved: " << img_count << std::endl << std::endl;
+    std::cout << "---------------------------------------------" << std::endl << std::endl;
     }
     return 0;
 }
