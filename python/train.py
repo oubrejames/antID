68faac661525abafe9e19b8e234ff7a24bad513e
James Oubre <oubrejames@gmail.com>
2023-07-05 16:35:48 -0500

    Trying both center cropped

--- python/train.py
+++ python/train.py
@@ -24,13 +24,14 @@ data_transforms = {
     'train': transforms.Compose([
         transforms.Resize(375),
         # transforms.RandomResizedCrop(224),
+        transforms.CenterCrop(375),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
         transforms.Resize(375),
-        # transforms.CenterCrop(224),
+        transforms.CenterCrop(375),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
