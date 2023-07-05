5929a8937ddea58fae37f1545dbf5863890553bd
James Oubre <oubrejames@gmail.com>
2023-07-05 17:01:08 -0500

    Testing removing normalization

--- python/train.py
+++ python/train.py
@@ -142,13 +142,13 @@ data_transforms = {
         transforms.CenterCrop(375),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
         transforms.Resize(375),
         transforms.CenterCrop(375),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
 }
 
