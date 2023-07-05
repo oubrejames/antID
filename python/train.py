066b63d8d7142af182b0f29ffde0d7f88b375d10
James Oubre <oubrejames@gmail.com>
2023-07-05 14:11:34 -0500

    Removed test imgs for now

--- python/train.py
+++ python/train.py
@@ -14,6 +14,7 @@ import time
 import os
 from PIL import Image
 from tempfile import TemporaryDirectory
+from torch import flatten
 
 cudnn.benchmark = True
 plt.ion()   # interactive mode
@@ -139,21 +140,21 @@ class ConvNeuralNet(nn.Module):
         
         ## Py img search
         # initialize first set of CONV => RELU => POOL layers
-        self.conv1 = Conv2d(in_channels=numChannels, out_channels=20,
+        self.conv1 = nn.Conv2d(in_channels=numChannels, out_channels=20,
             kernel_size=(5, 5))
-        self.relu1 = ReLU()
-        self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
         # initialize second set of CONV => RELU => POOL layers
-        self.conv2 = Conv2d(in_channels=20, out_channels=50,
+        self.conv2 = nn.Conv2d(in_channels=20, out_channels=50,
             kernel_size=(5, 5))
-        self.relu2 = ReLU()
-        self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
         # initialize first (and only) set of FC => RELU layers
-        self.fc1 = Linear(in_features=800, out_features=500)
-        self.relu3 = ReLU()
+        self.fc1 = nn.Linear(in_features=800, out_features=500)
+        self.relu3 = nn.ReLU()
         # initialize our softmax classifier
-        self.fc2 = Linear(in_features=500, out_features=classes)
-        self.logSoftmax = LogSoftmax(dim=1)
+        self.fc2 = nn.Linear(in_features=500, out_features=classes)
+        self.logSoftmax = nn.LogSoftmax(dim=1)
 
     # # Progresses data across layers
     # def __call__(self, x):
