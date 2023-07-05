b566ea64a3bfdf6f61027f1b5a7e0f9b4f24e279
James Oubre <oubrejames@gmail.com>
2023-07-05 16:14:00 -0500

    Changed cropping size for trainnig im

--- python/train.py
+++ python/train.py
@@ -22,15 +22,13 @@ plt.ion()   # interactive mode
 # Just normalization for validation
 data_transforms = {
     'train': transforms.Compose([
-        transforms.Resize(256),
-        transforms.RandomResizedCrop(224),
+        transforms.Resize(375),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize(375),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
