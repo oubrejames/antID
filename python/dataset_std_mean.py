c3043940cbed13d73bc1df28c1b3a84df107d3a3
James Oubre <oubrejames@gmail.com>
2023-07-05 19:54:09 -0500

    Made script to get dataset mean and std

---
+++ python/dataset_std_mean.py
@@ -0,0 +1,49 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+ 
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize(375),
+        transforms.CenterCrop(375),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(375),
+        transforms.CenterCrop(375),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = '../ant_face_data'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'val']}
+
+mean = 0.
+std = 0.
+nb_samples = 0.
+for loader in dataloaders:
+    for data in loader:
+        batch_samples = data.size(0)
+        data = data.view(batch_samples, data.size(1), -1)
+        mean += data.mean(2).sum(0)
+        std += data.std(2).sum(0)
+        nb_samples += batch_samples
+
+mean /= nb_samples
+std /= nb_samples
+
+print("mean: ", mean)
+print("std: ", std)
