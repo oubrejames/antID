11396e3ba3049883b863ccb16679c9b13b4a9b96
James Oubre <oubrejames@gmail.com>
2023-07-05 20:37:23 -0500

    Reverted norm params

--- python/train.py
+++ python/train.py
@@ -229,13 +229,13 @@ data_transforms = {
         transforms.CenterCrop(375),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize([-0.1839, -0.3908, -0.3304], [0.5050, 0.5857, 0.6100])
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
         transforms.Resize(375),
         transforms.CenterCrop(375),
         transforms.ToTensor(),
-        transforms.Normalize([-0.1839, -0.3908, -0.3304], [0.5050, 0.5857, 0.6100])
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
 }
 
