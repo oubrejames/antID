29be627413c74072b694bdcf1ca1de23eb639745
James Oubre <oubrejames@gmail.com>
2023-07-07 12:34:55 -0500

    Changed dataset output from dict to just images and label

--- python/datasets.py
+++ python/datasets.py
@@ -71,4 +71,4 @@ class TripletAntsDataset(Dataset):
             positive_image = self.transform(positive_image)
             negative_image = self.transform(negative_image)
 
-        return (anchor_image, positive_image, negative_image), self.labels.iloc[idx, 0]
+        return anchor_image, positive_image, negative_image, self.labels.iloc[idx, 0]
