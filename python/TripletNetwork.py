113df6f5db2d337777f6be91e0079dc6b3e488ee
oubrejames <jamesoubre2023@u.nothwestern.edu>
2023-07-18 14:55:30 -0500

    Changes to batch size and margin

--- python/TripletNetwork.py
+++ python/TripletNetwork.py
@@ -16,6 +16,9 @@ from datasets import TripletAntsDataset
 from networks import TripletNet, EmbeddingNet
 from trainer import fit_triplet
 from testing import test_model
+import shutil
+from plot_loss import plot_loss
+from torch.nn.parallel import DistributedDataParallel 
 
 cudnn.benchmark = True
 
@@ -44,42 +47,52 @@ data_transforms = {
 }
 
 
-# data_dir = '../ant_face_data'
-# csv_file = '../ant_face_data/labels.csv'
-data_dir = '../unseen_data'
-csv_file = '../unseen_data/labels.csv'
+data_dir = '../ant_face_data'
+csv_file = '../ant_face_data/labels.csv'
+
 dataset = TripletAntsDataset(csv_file, data_dir, transform=data_transforms['val'])
 
 train_size = int(0.8 * len(dataset))
 val_size = int(0.1 * len(dataset))
 test_size = len(dataset) - train_size - val_size
 
-# train_set, val_set, test_set = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
+train_set, val_set, test_set = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
 
-# train_loader = torch.utils.data.DataLoader(train_set, batch_size=100, shuffle=True, num_workers=4)
-# val_loader = torch.utils.data.DataLoader(val_set, batch_size=100, shuffle=True, num_workers=4)
-# test_loader = torch.utils.data.DataLoader(test_set, batch_size=100, shuffle=True, num_workers=4)
-test_loader = torch.utils.data.DataLoader(dataset, batch_size=100, shuffle=True, num_workers=4)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=100, shuffle=True, num_workers=4)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=100, shuffle=True, num_workers=4)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=100, shuffle=True, num_workers=4)
 
-# dataloaders = {'train': train_loader, 'val': val_loader, 'test': test_loader}
+dataloaders = {'train': train_loader, 'val': val_loader, 'test': test_loader}
 
 model = TripletNet(EmbeddingNet())
-criteria = nn.TripletMarginLoss(margin=1.0, p=2)
+criteria = nn.TripletMarginLoss(margin=0.7, p=2)
 optimizer = optim.Adam(model.parameters(), lr=0.01)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# model = nn.DataParallel(model, device_ids=[0, 1]) # Use both GPUs
+# model = DistributedDataParallel(model, device_ids=[[0, 1]])
 scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-# final_model, final_loss = fit_triplet(model, dataloaders, criteria, optimizer, scheduler, device, num_epochs=100)
+final_model, final_loss = fit_triplet(model, dataloaders, criteria, optimizer, scheduler, device, num_epochs=200)
 
-# loss_round = round(final_loss)
-# # Create final model name
-# final_model_path = "../models/best_loss" + str(loss_round) + ".pt"
+# Get model sequence number
+existing_models = os.listdir("../models")
+latest_model_seq = 0
+for existing_model in existing_models:
+    tmp_id = existing_model.split(".")[0][-1]
+    print("TMP ID: ", tmp_id)
+    if int(tmp_id) >= latest_model_seq:
+        latest_model_seq = int(tmp_id) + 1
 
-# # Save model
-# os.makedirs("../models", exist_ok=True)
-# torch.save(final_model.state_dict(), final_model_path)
 
+# Create final model name
+final_model_folder = "../models/triplet_net_" + str(latest_model_seq)
+final_model_path = final_model_folder + "/best_model.pt"
 
-model.load_state_dict(torch.load('../models/best_loss0.pt'))
-model.eval()
+# Save model
+os.makedirs("../models", exist_ok=True)
+os.makedirs(final_model_folder)
+torch.save(final_model.state_dict(), final_model_path)
 
-test_model(model, test_loader, device)
+# Copy the loss to folder
+path_to_loss = "../loss.csv"
+shutil.copy(path_to_loss, final_model_folder)
+plot_loss(final_model_folder)
