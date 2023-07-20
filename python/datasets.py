65b2d3b5600530ff7e3494a3618c9556fc31d01f
oubrejames <jamesoubre2023@u.nothwestern.edu>
2023-07-20 10:44:49 -0500

    Create new dataset to load ants and labels, bno triplets

--- python/datasets.py
+++ python/datasets.py
@@ -88,3 +88,37 @@ class TripletAntsDataset(Dataset):
             negative_image = self.transform(negative_image)
 
         return anchor_image, positive_image, negative_image, str(self.labels.iloc[idx, 0])
+
+class AntsDataset(Dataset):
+    """"""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Arguments:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.labels = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        anchor_path = os.path.join(self.root_dir,               # Directory to all ant folders
+                                self.labels.iloc[idx, 0],       # Directory to specific ant folder
+                                self.labels.iloc[idx, 1])       # Filename of anchor image
+
+        # anchor_image = io.imread(anchor_path)
+        anchor_image = Image.open(anchor_path)
+
+        if self.transform:
+            anchor_image = self.transform(anchor_image)
+
+        return anchor_image, str(self.labels.iloc[idx, 0])
