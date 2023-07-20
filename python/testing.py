5038bce21bf5b42e77b55bdb4ae99f178ec84cab
oubrejames <jamesoubre2023@u.nothwestern.edu>
2023-07-20 10:45:58 -0500

    Adding accuracy prints

--- python/testing.py
+++ python/testing.py
@@ -23,6 +23,9 @@ def test_model(model, test_loader, device, threshold):
     total_count = 0
     tested_count = 0
 
+    total_neg_dist= 0
+    total_pos_dist = 0
+
     while tested_count < len(test_loader):
         for anchor, positive, negative, label in test_loader:
             anchor, positive, negative = anchor.to(device), positive.to(device), negative.to(device)
@@ -31,6 +34,91 @@ def test_model(model, test_loader, device, threshold):
             # Calculate the squared L2 distance between each output
             anchor_positive_dist = torch.norm(anchor_output - positive_output)**2
             anchor_negative_dist = torch.norm(anchor_output - negative_output)**2
+
+            total_neg_dist += float(anchor_negative_dist)
+            total_pos_dist += float(anchor_positive_dist)
+
+            # Predict if positive and anchor are the same
+            if anchor_positive_dist < threshold:
+                # Anchor and positve are predicted the same
+                true_positive_count += 1
+            else:
+                false_negative_count += 1
+
+            if anchor_negative_dist > threshold:
+                true_negative_count += 1
+            else:
+                false_positive_count += 1
+
+            total_count += 1
+        
+        print("Avg pos dist for epoch: ", total_pos_dist/100)
+        print("Avg neg dist for epoch: ", total_neg_dist/100)
+        total_neg_dist= 0
+        total_pos_dist = 0
+        # True positive rate
+        tp_rate = 100*true_positive_count/(true_positive_count+false_positive_count)
+        tn_rate = 100*true_negative_count/(true_negative_count+false_negative_count)
+        fp_rate = 100*false_positive_count/(true_positive_count+false_positive_count)
+        fn_rate = 100*false_negative_count/(true_negative_count+false_negative_count)
+        accuracy = 100*(true_negative_count+true_positive_count)/(true_negative_count+\
+            true_positive_count+false_negative_count+false_positive_count)
+        print("TP Rate after ", tested_count, "epochs: ", tp_rate)
+        print("TN Rate after ", tested_count, "epochs: ", tn_rate)
+        print("FP Rate after ", tested_count, "epochs: ", fp_rate)
+        print("FN Rate after ", tested_count, "epochs: ", fn_rate)
+        print("Accuracy after", tested_count, "epochs: ", accuracy)
+        print("------------------------------------------------------------------- \n")
+        tested_count += 1
+
+    # True positive rate
+    tp_rate = 100*true_positive_count/(true_positive_count+false_positive_count)
+    tn_rate = 100*true_negative_count/(true_negative_count+false_negative_count)
+    fp_rate = 100*false_positive_count/(true_positive_count+false_positive_count)
+    fn_rate = 100*false_negative_count/(true_negative_count+false_negative_count)
+    accuracy = 100*(true_negative_count+true_positive_count)/(true_negative_count+\
+        true_positive_count+false_negative_count+false_positive_count)
+    print("TP Rate Final: ", tp_rate)
+    print("TN Rate Final: ", tn_rate)
+    print("FP Rate Final: ", fp_rate)
+    print("FN Rate Final: ", fn_rate)
+    print("Final Accuracy: ", accuracy)
+    print("Total number of testing images: ", total_count)
+    
+
+    return None
+
+def test_thresholds(model, test_loader, device, path_to_folder):
+    model = model.to(device)
+    true_positive_count = 0
+    true_negative_count = 0
+    false_positive_count = 0
+    false_negative_count = 0
+    total_count = 0
+    tested_count = 0
+    epoch_count = 0
+    threshold = 300
+    thresh_vals = []
+    tp_vals = []
+    tn_vals = []
+    best_threshold = threshold
+    best_tp_rate = 0
+    best_tn_rate = 0
+
+    total_neg_dist = 0
+    total_pos_dist = 0
+
+    while tested_count < len(test_loader):
+        for anchor, positive, negative, label in test_loader:
+            anchor, positive, negative = anchor.to(device), positive.to(device), negative.to(device)
+            anchor_output, positive_output, negative_output = model(anchor, positive, negative)
+
+            # Calculate the squared L2 distance between each output
+            anchor_positive_dist = torch.norm(anchor_output - positive_output)**2
+            anchor_negative_dist = torch.norm(anchor_output - negative_output)**2
+
+            total_neg_dist += float(anchor_negative_dist)
+            total_pos_dist += float(anchor_positive_dist)
             # print("Pos dist: ", anchor_positive_dist)
             # print("Neg dist: ", anchor_negative_dist)
             # Predict if positive and anchor are the same
@@ -46,87 +134,19 @@ def test_model(model, test_loader, device, threshold):
                 false_positive_count += 1
 
             total_count += 1
-            # True positive rate
-        tp_rate = 100*true_positive_count/total_count
-        tn_rate = 100*true_negative_count/total_count
-        fp_rate = 100*false_positive_count/total_count
-        fn_rate = 100*false_negative_count/total_count
-        print("TP Rate after ", tested_count, "epochs: ", tp_rate)
-        print("TN Rate after ", tested_count, "epochs: ", tn_rate)
-        print("FP Rate after ", tested_count, "epochs: ", fp_rate)
-        print("FN Rate after ", tested_count, "epochs: ", fn_rate)
-        print("------------------------------------------------------------------- \n")
-        tested_count += 1
+            epoch_count += 1
 
-    # True positive rate
-    tp_rate = 100*true_positive_count/total_count
-    tn_rate = 100*true_negative_count/total_count
-    fp_rate = 100*false_positive_count/total_count
-    fn_rate = 100*false_negative_count/total_count
-    print("TP Rate Final: ", tp_rate)
-    print("TN Rate Final: ", tn_rate)
-    print("FP Rate Final: ", fp_rate)
-    print("FN Rate Final: ", fn_rate)
-    print("Total number of testing images: ", total_count)
-    
+        # Caclulate metrics for each epoch tested
+        tp_rate = 100*true_positive_count/epoch_count
+        tn_rate = 100*true_negative_count/epoch_count
+        fp_rate = 100*false_positive_count/epoch_count
+        fn_rate = 100*false_negative_count/epoch_count
 
-    return None
-
-def test_thresholds(model, test_loader, device, path_to_folder):
-    model = model.to(device)
-    true_positive_count = 0
-    true_negative_count = 0
-    false_positive_count = 0
-    false_negative_count = 0
-    total_count = 0
-    tested_count = 0
-
-    threshold = 200
-    thresh_vals = []
-    tp_vals = []
-    tn_vals = []
-    best_threshold = threshold
-    best_tp_rate = 0
-    best_tn_rate = 0
-
-    total_neg_dist = 0
-    total_pos_dist = 0
-
-    while tested_count < len(test_loader):
-        for anchor, positive, negative, label in test_loader:
-            print("Total count: ",total_count)
-            anchor, positive, negative = anchor.to(device), positive.to(device), negative.to(device)
-            anchor_output, positive_output, negative_output = model(anchor, positive, negative)
-
-            # Calculate the squared L2 distance between each output
-            anchor_positive_dist = torch.norm(anchor_output - positive_output)**2
-            anchor_negative_dist = torch.norm(anchor_output - negative_output)**2
-
-            total_neg_dist += anchor_negative_dist
-            total_pos_dist += anchor_positive_dist
-
-            # Predict if positive and anchor are the same
-            if anchor_positive_dist < threshold:
-                # Anchor and positve are predicted the same
-                true_positive_count += 1
-            else:
-                false_negative_count += 1
-
-            if anchor_negative_dist > threshold:
-                true_negative_count += 1
-            else:
-                false_positive_count += 1
-
-            total_count += 1
-            # True positive rate
-        tp_rate = 100*true_positive_count/total_count
-        tn_rate = 100*true_negative_count/total_count
-        fp_rate = 100*false_positive_count/total_count
-        fn_rate = 100*false_negative_count/total_count
+        # Save threshold 
         thresh_vals.append(threshold)
         tp_vals.append(tp_rate)
         tn_vals.append(tn_rate)
-        total_count = 0
+        epoch_count = 0
         true_positive_count = 0
         true_negative_count = 0
         false_negative_count = 0
@@ -138,15 +158,17 @@ def test_thresholds(model, test_loader, device, path_to_folder):
         print("FN Rate at threshold = ", threshold, ": ", fn_rate)
         print("------------------------------------------------------------------- \n")
         tested_count += 1
-        print("Tested: ",tested_count)
         # Update best threshold
         if (tp_rate+tn_rate)/2 > (best_tp_rate+best_tn_rate)/2:
             best_threshold = threshold
             best_tn_rate = tn_rate
             best_tp_rate = tp_rate
 
-    average_pos_dist = total_pos_dist/(tested_count+1)
-    average_neg_dist = total_neg_dist/(tested_count+1)
+    average_pos_dist = total_pos_dist/(total_count+1)
+    average_neg_dist = total_neg_dist/(total_count+1)
+    print("Average positive dist: ", average_pos_dist)
+    print("Average negative dist: ", average_neg_dist)
+
     average_thresh = average_pos_dist + (average_neg_dist - average_pos_dist)/2
     plt.plot(thresh_vals, tp_vals, color = 'r', label = "tp_rate")
     plt.plot(thresh_vals, tn_vals, color = 'g', label = "tn_rate")
@@ -155,4 +177,4 @@ def test_thresholds(model, test_loader, device, path_to_folder):
     plt.legend()
     plt.savefig(path_to_folder + '/thresh_test.png')
 
-    return best_threshold, average_thresh
+    return average_thresh
