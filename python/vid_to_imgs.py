9a34c959a600bd227eacee29390be841a5017dcb
James Oubre <oubrejames@gmail.com>
2023-07-23 15:16:21 -0500

    Added docstrings

--- python/vid_to_imgs.py
+++ python/vid_to_imgs.py
@@ -4,11 +4,19 @@ from roboflow import Roboflow
 import os
 from ultralytics import YOLO
 
-
+# Import yolo model
 model= YOLO("YOLO_V8/runs/detect/yolov8s_v8_25e6/weights/best.pt")
 
 def get_head_box(im):
-    # detections = model.predict(im, confidence=60, overlap=30).json()
+    """Take a video frame and return the bounding box of the ant head if it exists.
+
+    Args:
+        im (CV::MAT): Input image frame
+
+    Returns:
+        list: Two points idescribing the bounding box of the ant head
+    """
+
     detections = model(im, conf=0.8)
     bboxes = detections[0].boxes
     num_detections = bboxes.xyxy.size(dim=0)
@@ -16,22 +24,28 @@ def get_head_box(im):
     if num_detections > 0:
         detection = detections[0]
         bbox = detection.boxes
-        x1 = bbox.xyxy[0][0] #int(detection['x']) - int(detection['width'] / 2)
-        x2 = bbox.xyxy[0][2] #int(detection['x']) + int(detection['width'] / 2)
-        y1 = bbox.xyxy[0][1] #int(detection['y']) - int(detection['height'] / 2)
-        y2 = bbox.xyxy[0][3] #int(detection['y']) + int(detection['height'] / 2)
+        x1 = bbox.xyxy[0][0]
+        x2 = bbox.xyxy[0][2]
+        y1 = bbox.xyxy[0][1]
+        y2 = bbox.xyxy[0][3]
         box = [[x1, y1], [x2, y2]]
         return box
     else:
         return None
 
 def main():
+    """
+    This script loops through all videos in a directory of labeled videos, performs object detection on
+    each frame, crops the image to an ant head, and saves the cropped image to a directory of labeled 
+    images.
+    """
+
     # Initialize counter for image names
     img_cnt = 0
-    
+
     # Loop through all videos in labeled_videos
     videos_directory = "../labeled_vids"
-    # videos_directory = "../unseen_vids"
+    # videos_directory = "../unseen_vids" # Two video directories to make training and testing data
 
     print("Processing videos in " + videos_directory)
     for ant_video in os.listdir(videos_directory):
@@ -40,16 +54,14 @@ def main():
         # Create path to save images
         ant_id = path_to_video.split("/")[-1].split(".")[0]
         path_to_imgs = "../labeled_images/" + ant_id
-        # path_to_imgs = "../unseen_images/" + ant_id
+        # path_to_imgs = "../unseen_images/" + ant_id # Must change images_directory for unseen_images
 
         # Check if directory already exists
         if os.path.exists(path_to_imgs):
             print("Images for " + ant_id + " already exist. Skipping...")
             continue
-        
+
         os.mkdir(path_to_imgs)
-
-
 
         # Open video with OpenCV
         cap= cv2.VideoCapture(path_to_video)
