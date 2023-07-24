aa73d66c9148b84ae15d45972a91c70fe98040e2
James Oubre <oubrejames@gmail.com>
2023-07-24 16:24:14 -0500

    IMplimented most of HDBSCAN, pushing to see if it works and edit on lambda

--- python/cluster.py
+++ python/cluster.py
@@ -1,106 +1,60 @@
-from trainer import fit
+from sklearn.cluster import HDBSCAN
+from datasets import AntsDataset
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import torch.backends.cudnn as cudnn
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
-import time
-import os
-from PIL import Image
-from tempfile import TemporaryDirectory
-from datasets import TripletAntsDataset, AntsDataset
-from networks import TripletNet, EmbeddingNet
-from trainer import fit_triplet
-from testing import test_model, test_thresholds
+from torchvision import transforms
+from networks import TripletNet, EmbeddingNet, FaceNet
 
-cudnn.benchmark = True
+######### PARAMETERS #########
+embedding_network = EmbeddingNet()
+batch_size = 100
+model_number = 22
+gpu_id = "cuda:1"
+gpu_parallel = False
+##############################
 
-# Data augmentation and normalization for training
-# Just normalization for validation
-data_transforms = {
-    'crop_norm': transforms.Compose([
-        transforms.Resize(375),
-        transforms.CenterCrop(375),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-}
+# Resize and normalize the images
+data_transforms = transforms.Compose([
+                transforms.Resize(375),
+                transforms.CenterCrop(375),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
 
-data_dir = '../unseen_data'
-csv_file = '../unseen_data/labels.csv'
-dataset = AntsDataset(csv_file, data_dir, transform=data_transforms['crop_norm'])
+# Load dataset
+unseen_dir = '../unseen_data'
+unseen_csv = '../unseen_data/labels.csv'
+unseen_dataset = AntsDataset(unseen_csv, unseen_dir, transform=data_transforms)
+unseen_test_loader = torch.utils.data.DataLoader(unseen_dataset, batch_size=100, shuffle=True, num_workers=4)
 
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=100, shuffle=False, num_workers=4)
+# Load model
+model = TripletNet(embedding_network)
+device = torch.device(gpu_id if torch.cuda.is_available() else "cpu")
+
+if gpu_parallel:
+    model = nn.DataParallel(model, device_ids=[0,1]) # Use both GPUs
 
 
-model = TripletNet(EmbeddingNet())
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-# model = nn.DataParallel(model, device_ids=[0, 1]) # Use both GPUs
-
-model.load_state_dict(torch.load('../models/triplet_net_9/best_model.pt'))
+# Load trained model and put into evaluation mode
+trained_model_path = '../models/triplet_net_' + str(model_number) + '/best_model.pt'
+model.load_state_dict(torch.load(trained_model_path, map_location=device))
 model.eval()
-model.to(device)
 
-# img_path = '../unseen_images/ant_17/im289.jpg'
-# img = Image.open(img_path)
-# img = dataset.transform(img)
-# print("IMAGE SHAPE: ", img.shape)
-# img = img.to(device)
+# Declare array to hold embeddings
+embeddings = []
+labels = []
 
-# embedding = model.get_embedding(img)
-# print("EMBD SHAPE: ". embedding.shape)
-
-embd_label_arr = []
-
-# Loop through every ant and save embedding and label to a list in form [[embd, lbl], ...]
-for images, labels in dataloader:
-    images = images.to(device)
-    print("Image shape: ", images.shape)
-    embedding = model(images, images, images)
-    print("tensor shape:",embedding[0].shape)
-    # print("tensor shape:",embedding[1].shape)
-    # print("tensor shape:",embedding[2].shape)
-
-    embedding = model.get_embedding(image).cpu().detach().numpy()
-    print("embedding shape:",embedding.shape)
-    embd_label_arr.append([embedding, label])
-
-# embd_classes = []
-# pred_embd_label_arr = []
-
-# # Loop through all embeddings, compare current embedding from embd_label_arr to each embedding class
-# # in embd_classes
-# # If the current embedding is a positive match with a class embedding then add to list [[embd, label, class_pred],...]
-# # and update the class embedding to average the two 
-# new_class = True
-# num_classes = 0
-# for elm_pair in embd_label_arr:
-#     embedding = elm_pair[0]
-#     label = elm_pair[1]
+# Loop through all images in the dataset and get the embeddings
+for images, labels in unseen_test_loader:
     
-#     # Loop through each embedding class
-#     if embd_classes:
-#         for class_embd_pair in embd_classes:
-#             class_embedding = class_embd_pair[0]
-#             embedding_class = class_embd_pair[1]
-            
-#             # Get the norm dist between the embedding and class embedding
-#             dist_btw_embd = np.linalg.norm(embedding - class_embedding)**2
+    # Save embeddings to an array of size (n_samples, n_features)
+    for i in range(len(images)):
+        image = images[i]
+        label = labels[i]
+        embeddings.append(model.get_embedding(image).detach().numpy())
+        labels.append(label)
 
-#             if dist_btw_embd < 500: # If there is a match TODO check if you get more than one match
-#                 pred_embd_label_arr.append([embedding, label, embedding_class])
-#                 print('POSITIVE')
-#                 class_embd_pair[0] = (class_embd_pair[0]+embedding)/2
-#                 new_class = False
-#     else:
-#         new_class = True
-    
-#     if new_class:
-#         embd_classes.append([embedding, num_classes])
-#         num_classes += 1
-#         pred_embd_label_arr.append([embedding, label, num_classes])
+# Run HDBSCAN on the embeddings
+hdb = HDBSCAN(min_cluster_size=10, min_samples=1)
+hdb.fit(embeddings)
+hdb_labels = hdb.labels_
