ecd9692d529ccd745f35860c7f7d22426fab439e
oubrejames <jamesoubre2023@u.nothwestern.edu>
2023-07-26 13:27:05 -0500

    Added yolo network to see layers for transfer

--- python/model_sum.py
+++ python/model_sum.py
@@ -1,19 +1,25 @@
 from torchvision import models
 from torchsummary import summary
-from networks import TripletNet, EmbeddingNet
-
+from networks import TripletNet, EmbeddingNet, TransferYOLO
+from ultralytics import YOLO
+import torch
 """
 Print out the model architecture for the a given model.
 """
 
-print('Embedding net: \n ----------------------------')
-model_e = EmbeddingNet()
-summary(model_e, (3, 375, 375))
+# print('Embedding net: \n ----------------------------')
+# model_e = EmbeddingNet()
+# summary(model_e, (3, 375, 375))
 
-print('\n Triplet net: \n ----------------------------')
+# print('\n Triplet net: \n ----------------------------')
 
-model = TripletNet(EmbeddingNet())
-summary(model,((3, 375, 375), (3, 375, 375), (3, 375, 375)))
+# model = TripletNet(EmbeddingNet())
+# summary(model,((3, 375, 375), (3, 375, 375), (3, 375, 375)))
 
-print('\n Printed Model \n --------------------------------')
-print(model_e)
+# print('\n Printed Model \n --------------------------------')
+# print(model_e)
+
+model = TransferYOLO()
+print(model)
+
+summary(model, (3,375,375))
