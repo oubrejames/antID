49366f64440f2c8c0ea917e6574c2ec1bbc57736
oubrejames <jamesoubre2023@u.nothwestern.edu>
2023-08-08 12:21:02 -0500

    Just changed params for training and added some commentsfor keeping track of best models

--- python/TripletNetwork.py
+++ python/TripletNetwork.py
@@ -8,7 +8,7 @@ import numpy as np
 from torchvision import transforms
 import os
 from datasets import TripletAntsDataset
-from networks import TripletNet, EmbeddingNet, FaceNet, EN2, TransferYOLO, EN3
+from networks import TripletNet, EmbeddingNet, FaceNet, EN2, TransferYOLO, EN3, CNN_EN, EN4, EN5
 from trainer import fit_triplet
 from tester import test_model
 import shutil
@@ -21,19 +21,22 @@ This script trains a triplet network on the ant face dataset.
 """
 
 ######### PARAMETERS #########
-embedding_network = EN3()
+embedding_network = EN4()
 batch_size = 100
-loss_margin = 0.5
+loss_margin = 1 # 79.4 at 0,7 .... 79.6 at 1 (both with batch of 70) ... 78 for 0.5 bstch 100 ... 77 for 1.5 all behind is for en2
 loss_p = 2
-learn_rate = 0.01
-gpu_id = "cuda:1"
+learn_rate = 0.0001
+gpu_id = "cuda:0"
 gpu_parallel = False
 scheduler_step_size = 7
 scheduler_gamma = 0.1
-num_epochs = 200
-early_stopper = EarlyStopper(patience=15, min_delta=0.001)
+num_epochs = 100
+early_stopper = EarlyStopper(patience=7, min_delta=0.0)
 ##############################
 
+## EN4 Best params (somwhere around 46 is best)
+## Batch 100, margin 1 or 0.7, lr 0.,0001, output layer 128 
+## Batch 100 EN5, margin 1, lr 0.0001, lossp 2
 
 # Enable benchmarking for faster runtime
 cudnn.benchmark = True
@@ -49,65 +52,11 @@ data_transforms = transforms.Compose([
 
 
 
-#################################################################################################
-#################################################################################################
-#################### TESTING WITH BALANCED DATA #############################
-#################################################################################################
-#################################################################################################
-
-# #Upload the data and save it to a Dataframe.
-# df = pd.read_csv( '../ant_face_data/labels.csv', header=None)
-# df=pd.DataFrame(df)
-# print("df size: ", df.shape)
-
-# # Check the class distribution. (Hint: Use .value_counts attribute and 
-# # remember that the labels are at the last column of the dataframe)
-# results = df[0]
-
-
-# # Ants 11, 12, 16, and 26 have much more instances than the other training data and ant 8 has barely
-# # any.
-# # Make the dataset more balanced by sampling ants 11, 12, 16, and 26 and removing ant 8
-# missing_ants = df.drop(df[df[0] == 'ant_11'])
-# # The value you want to remove from the DataFrame
-# target_classes = ["ant_11", "ant_12", "ant_16", "ant_26", "ant_8"]
-
-# filtered_df = df
-# for target_class in target_classes:
-#     # Boolean indexing to filter out rows with the target_class
-#     filtered_df = filtered_df[filtered_df[0] != target_class]
-
-# # print(missing_ants)
-# # missing_ants.drop(missing_ants[missing_ants[0] == 'ant_12'])
-# # missing_ants = missing_ants.drop(missing_ants[missing_ants[0] == 'ant_16'])
-# # missing_ants = missing_ants.drop(missing_ants[missing_ants[0] == 'ant_26'])
-# # missing_ants = missing_ants.drop(missing_ants[missing_ants[0] == 'ant_8'])
-
-# all_11 = df[df[0] == 'ant_11']
-# all_12 = df[df[0] == 'ant_12']
-# all_16 = df[df[0] == 'ant_16']
-# all_26 = df[df[0] == 'ant_26']
-
-# smaller_11 = all_11.sample(860)
-# smaller_12 = all_12.sample(860)
-# smaller_16 = all_16.sample(860)
-# smaller_26 = all_26.sample(860)
-
-# # # balanced_df = balanced_df.drop(df[df[0] == 'ant_12'].sample(860).index)
-# # # balanced_df = balanced_df.drop(df[df[0] == 'ant_16'].sample(860).index)
-# # # balanced_df = balanced_df.drop(df[df[0] == 'ant_26'].sample(860).index)
-# # # balanced_df = balanced_df.drop(df[df[0] == 'ant_8'])
-# balanced_df = pd.concat([smaller_11, smaller_12, smaller_16, smaller_26, filtered_df], axis=0)
-# csv_file = balanced_df
-#################################################################################################
-#################################################################################################
-#################################################################################################
-#################################################################################################
-
-
 # Create dataset
-data_dir = '../ant_face_data'
-csv_file = '../ant_face_data/labels.csv'
+# data_dir = '../ant_face_data'
+# csv_file = '../ant_face_data/labels.csv'
+data_dir = '../ant_body_data'
+csv_file = '../ant_body_data/labels.csv'
 dataset = TripletAntsDataset(csv_file, data_dir, transform=data_transforms)
 
 
@@ -152,7 +101,7 @@ for existing_model in existing_models:
 
 
 # Create final model name
-final_model_folder = "../models/triplet_net_" + str(latest_model_seq)
+final_model_folder = "../models/triplet_net_body_" + str(latest_model_seq)
 final_model_path = final_model_folder + "/best_model.pt"
 
 
